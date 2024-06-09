3bb93557ec80bd1b20c090f0e236ac85fe61c3f9
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-06-09 15:11:31 -0600

    [Update MismatchError]

--- Grafo.java
+++ Grafo.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 public class Grafo {
     private List<Vertice> listaVertices;
     public void agregarNodo(Vertice verticeNuevo){
@@ -18,27 +21,63 @@ public class Grafo {
             for (int i = 0; i < listaVertices.size(); i++) {
                 System.out.println( i + 1 + ") " + listaVertices.get(i).getDato());
             }
-            System.out.println("SELECCIONE EL NUMERO DEL VERTICE ORIGEN: " );
-            opcion = entrada.nextInt();
+            do{
+                if (count != 0) {
+                    System.out.println("-----debe ser un indice mostrado en el menu-----");
+                }
+                System.out.print("SELECCIONE EL NUMERO DEL VERTICE ORIGEN: " );
+                opcion = decidir();
+                count ++;
+            }while(opcion >= listaVertices.size()+1);
+            count = 0;
             origen = listaVertices.get( opcion - 1);
-            System.out.println("INDIQUE EL NUMERO DEL VERTICE DESTINO: " );
-            opcion = entrada.nextInt();
+            do{
+                if (count != 0) {
+                    System.out.println("-----debe ser un indice mostrado en el menu-----");
+                }
+                System.out.print("INDIQUE EL NUMERO DEL VERTICE DESTINO: " );
+                opcion = decidir();
+                count ++;
+            }while(opcion >= listaVertices.size()+1);
+            count = 0;
             destino = listaVertices.get( opcion - 1);
             do{
                 if (count != 0) {
                     System.out.println("-----EL PESO DEBE SER MAYOR A CERO-----");
                 }
-                System.out.println("INDIQUE EL PESO: ");
-                peso = entrada.nextInt();
+                System.out.print("INDIQUE EL PESO: ");
+                peso = decidir();
                 count++;
             }while (peso <= 0);
             origen.setListaAristas(new Arista(origen, destino, peso));
-            System.out.println("agregar mas aristas? 1. Si\t 2. No");
-            opcion = entrada.nextInt();
+            System.out.print("agregar mas aristas (1. Si\t 2. No)? ");
+            opcion = decidir();
         }while(opcion == 1);
     }
     @Override
     public String toString (){
         return " GRAFO [ " + getListaVertices() + "\n ]";
+    }
+    public int decidir(){
+        int indice = 0;
+        boolean excepcion = true;
+        do{
+            try{
+                indice = validarEleccion();
+                excepcion = false;
+            }catch(InputMismatchException e){
+                System.out.println("----------¡Debe ser un numero entero!--------\\n");
+                System.out.print("vuelva a introducir la opcion: ");
+            }
+            
+        }while(excepcion);
+        
+        return indice;
+    } 
+
+    public int validarEleccion() throws InputMismatchException{
+        Scanner entrada = new Scanner(System.in);
+        int eleccion = entrada.nextInt();
+        return eleccion;
     }
 }
--- Principal.java
+++ Principal.java
@@ -1,11 +1,12 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Principal{
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int cantidad;
         Grafo grafo = new Grafo();
-        System.out.println("indica la cantidad de vertices del grafo: ");
-        cantidad = entrada.nextInt();
+        System.out.print("\nindica la cantidad de vertices del grafo: ");
+        cantidad = decidir();
         for (int i = 0; i < cantidad; i++) {
             grafo.agregarNodo(crearVertice(grafo));
         }
@@ -14,8 +15,31 @@ public class Principal{
     }
     public static Vertice crearVertice(Grafo grafo){
         Scanner entrada = new Scanner(System.in);
-        System.out.println("ingresa el valor del vertice: ");
+        System.out.print("ingresa el valor del vertice: ");
         Vertice nuevoVertice = new Vertice(entrada.next());
         return nuevoVertice;
+    }
+    public static int decidir(){
+        int indice = 0;
+        boolean excepcion = true;
+        do{
+            try{
+                
+                indice = validarEleccion();
+                excepcion = false;
+            }catch(InputMismatchException e){
+                System.out.println("----------¡Debe ser un numero entero!--------\n");
+                System.out.print("vuelva a introducir la opcion: ");
+            }
+            
+        }while(excepcion);
+        
+        return indice;
+    } 
+
+    public static int validarEleccion() throws InputMismatchException{
+        Scanner entrada = new Scanner(System.in);
+        int eleccion = entrada.nextInt();
+        return eleccion;
     }
 }
